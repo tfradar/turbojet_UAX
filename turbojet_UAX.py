b78ed23ee3e16c53229ca376d73aa2df2f0f8590
Toni Frau Darder <ToniFrau@ucm116156.wifi.ucm.es>
2017-05-18 19:44:47 +0200

    se ha quitado el lector de texto y directamente es capaz de imprimir gastos corregidos

--- turbojet_UAX.py
+++ turbojet_UAX.py
@@ -1,28 +1,22 @@
-import numpy as np
-from matplotlib import  pyplot as plt
+""
+# Este .py se encargará de llamar a los datos clasificados y trabajar con ellos
 
-def gasto_corregido(G, T, p):
-    Tsl = 288 # Grados K, Temp estandar
-    psl = 101325 # Pa, pres estandar
+import numpy as np
+import cargadatos_turbojet_UAX as datos
+
+
+# Función de Gasto corregido
+def gasto_corregido(G):
+    Tsl = 288           # K, Temp estandar
+    psl = 101325        # Pa, pres estandar
+    p = 93832.2187500   # Pa, pres ese día
+    T = 302             # K, Temp ese día
     theta = T / Tsl
     delta = p / psl
     Gc = G * (np.sqrt(theta) / delta)
 
     return Gc
 
-datos_arrancada = np.loadtxt('Datos/arrancada_36000.ASC', comments='#')
-#t[s] P0 [Pa]	P2 [Pa]	P3t [Pa]	P4t [Pa]	P5t [Pa]	P6t [Pa]	T2t [K]	T3t [K]	T4t1 [K]	T4t2 [K]	T5t1 [K]	T5t2 [K]	T8t [K]	Empuje [N]	fuel [kg/s]	aire [kg/s]	/s]
-tiempo = datos_arrancada[:,0]
-p3t = datos_arrancada[:,3]
-E = datos_arrancada[:,14]
-T4t1 = datos_arrancada[:,9]
-T4t2 = datos_arrancada[:,10]
-c = datos_arrancada[:,15]
-G = datos_arrancada[:,16]
+print(datos.G_108000)
+print(gasto_corregido(datos.G_108000))
 
-
-plt.figure()
-plt.plot(tiempo,p3t)
-plt.xlabel('tiempo [s]')
-plt.ylabel('p3t')
-plt.show()
